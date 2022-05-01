91db86afe5ef42658046a5e34c2864cf02adc2b0
Andrea Dragotta <dev@andreadev.it>
2022-05-01 23:17:41 +0200

    renamed folder for import (timetrap was too much generic)

--- lua/timetrap_nvim/utils.lua
+++ lua/timetrap_nvim/utils.lua
@@ -49,6 +49,44 @@ local splitLines = function (str)
     return lines
 end
 
+local parseRecordLine = function (buf, cursor)
+    -- Get info positions in buffer (headers are in the second line)
+    local headers = vim.api.nvim_buf_get_lines(buf, 1, 2, false)
+
+    local r, _ = unpack(cursor)
+    local cur_line = vim.api.nvim_buf_get_lines(buf, r, r+1, false)
+
+    local first_char = cur_line:sub(1,1)
+    -- If the first character of the string is a space, it is not a record
+    if first_char == " " then
+        return
+    end
+
+    -- And neither is a record if the first character is not a number
+    if tonumber(first_char, 10) == nil then
+        return
+    end
+
+    -- Get the index at which I should split the line (extracted from the headers)
+    local was_space = false
+    local headers_split = {}
+    for i=1, #headers do
+        local c = headers:sub(i, i)
+        if c ~= " " then
+            if was_space then
+                table.insert(headers_split, i)
+                was_space = false
+            end
+        else
+            was_space = true
+        end
+    end
+
+    -- Split the line and return the info as a table
+
+
+
+end
 
 -- Module exports
 local M = {}
@@ -63,5 +101,7 @@ M.prompt = function (display_text, type, on_submit, on_close)
 end
 
 M.splitLines = splitLines
+
+M.parseRecordLine = parseRecordLine
 
 return M
